c3324da5b833897f89b6efa154f0855dc49c83c4
lookchesnok <lookchesnok@gmail.com>
2018-08-25 22:13:31 +0700

    Добавил управление с пульта.

--- clockOnLed/clockOnLed.ino
+++ clockOnLed/clockOnLed.ino
@@ -5,7 +5,7 @@
 #include "small4pt7b.h"
 #include <IRremote.h>
 #define LED_PIN 8
-//#define RECV_PIN 11
+#define RECV_PIN 11
 
 Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(21, 11, LED_PIN,
 	NEO_MATRIX_TOP + NEO_MATRIX_LEFT +
@@ -15,19 +15,19 @@ Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(21, 11, LED_PIN,
 DS3231 clock;
 RTCDateTime dt;
 
-//IRrecv irrecv(RECV_PIN);
+IRrecv irrecv(RECV_PIN);
 
-//decode_results results;
+decode_results results;
 const uint16_t colors[] = {
  matrix.Color(255, 0, 0), matrix.Color(0, 255, 0), matrix.Color(0, 0, 255) };
 
 void setup() {
-	// Serial.begin(9600);
-	 // In case the interrupt driver crashes on setup, give a clue
-	 // to the user what's going on.
-	// Serial.println("Enabling IRin");
-	// irrecv.enableIRIn(); // Start the receiver
-	// Serial.println("Enabled IRin");
+	Serial.begin(9600);
+	// In case the interrupt driver crashes on setup, give a clue
+	// to the user what's going on.
+	Serial.println("Enabling IRin");
+	irrecv.enableIRIn(); // Start the receiver
+	Serial.println("Enabled IRin");
 
 	clock.begin();
 	// clock.setDateTime(DATE, TIME);
@@ -39,28 +39,53 @@ void setup() {
 	matrix.setFont(&small_5x34pt7b);
 }
 
+const unsigned long upCommand = 16736925; //0xFF629D
+const unsigned long leftCommand = 16720605; //0xFF22DD
+const unsigned long rightCommand = 16761405; //0xFFC23D
+const unsigned long downCommand = 16754775; // 0xFFA857
+
+bool showTime = true;
+bool showDate = false;
+
 void loop() {
-	// if (irrecv.decode(&results)) {
-	//   Serial.println(results.value, HEX);
-	//   irrecv.resume(); // Receive the next value
-   //  }
+	if (irrecv.decode(&results)) {
+		switch (results.value)
+		{
+		case upCommand:
+			showDate = true;
+			break;
+		case downCommand:
+			showDate = false;
+			break;
+		default:
+			break;
+		}
+
+		irrecv.resume(); // Receive the next value
+	}
 
 	matrix.fillScreen(0);
 	dt = clock.getDateTime();
-	matrix.setTextColor(0x2E3F);
-	matrix.setCursor(2, 4);
-	matrix.print(clock.dateFormat("H", dt));
-	matrix.setCursor(10, 4);
-	if ((dt.second % 2) == 1)
-	{
-		matrix.print(':');
-	}
-	matrix.setCursor(12, 4);
-	matrix.print(clock.dateFormat("i", dt));
-	matrix.setTextColor(0x8E84);
-	matrix.setCursor(2, 10);
-	matrix.print(clock.dateFormat("d.m", dt));
 
+	if (showTime)
+	{
+		matrix.setTextColor(0x2E3F);
+		matrix.setCursor(2, 4);
+		matrix.print(clock.dateFormat("H", dt));
+		matrix.setCursor(10, 4);
+		if (dt.second % 2 == 1)
+		{
+			matrix.print(':');
+		}
+		matrix.setCursor(12, 4);
+		matrix.print(clock.dateFormat("i", dt));
+	}
+	if (showDate)
+	{
+		matrix.setTextColor(0x8E84);
+		matrix.setCursor(2, 10);
+		matrix.print(clock.dateFormat("d.m", dt));
+	}
 	matrix.show();
 	delay(100);
 }
