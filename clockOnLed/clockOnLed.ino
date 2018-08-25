ab5c583d7d81b5d46256afaab27e5e9544fc33ec
lookchesnok <lookchesnok@gmail.com>
2018-08-25 20:54:29 +0700

    Анимация разделителя времени

--- clockOnLed/clockOnLed.ino
+++ clockOnLed/clockOnLed.ino
@@ -3,48 +3,64 @@
 #include <Adafruit_NeoPixel.h>
 #include <DS3231.h>
 #include "small4pt7b.h"
-#include <IRremote.h>
-#define PIN 3
-IRrecv irrecv(2); // указываем вывод, к которому подключен приемник
-decode_results results;
+//#include <IRremote.h>
+#define LED 8
+//#define RECV_PIN 11
 
-
-Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(21, 11, PIN,
-  NEO_MATRIX_TOP     + NEO_MATRIX_LEFT +
-  NEO_MATRIX_ROWS + NEO_MATRIX_ZIGZAG,
-  NEO_GRB            + NEO_KHZ800);
+Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(21, 11, LED,
+	NEO_MATRIX_TOP + NEO_MATRIX_LEFT +
+	NEO_MATRIX_ROWS + NEO_MATRIX_ZIGZAG,
+	NEO_GRB + NEO_KHZ800);
 
 DS3231 clock;
 RTCDateTime dt;
 
+//IRrecv irrecv(RECV_PIN);
+
+//decode_results results;
 const uint16_t colors[] = {
  matrix.Color(255, 0, 0), matrix.Color(0, 255, 0), matrix.Color(0, 0, 255) };
 
 void setup() {
-//  Serial.begin(9600); // выставляем скорость COM порта
-//  irrecv.enableIRIn(); // запускаем прием
-  clock.begin();
-// clock.setDateTime(DATE, TIME);
+	// Serial.begin(9600);
+	 // In case the interrupt driver crashes on setup, give a clue
+	 // to the user what's going on.
+	// Serial.println("Enabling IRin");
+	// irrecv.enableIRIn(); // Start the receiver
+	// Serial.println("Enabled IRin");
 
-  matrix.begin();
-  matrix.setTextWrap(false);
-  matrix.setBrightness(5);
-  matrix.setTextColor(colors[0]);
-  matrix.setFont(&small_5x34pt7b);
+	clock.begin();
+	// clock.setDateTime(DATE, TIME);
+
+	matrix.begin();
+	matrix.setTextWrap(false);
+	matrix.setBrightness(5);
+	matrix.setTextColor(colors[0]);
+	matrix.setFont(&small_5x34pt7b);
 }
 
 void loop() {
-  
- 
-  matrix.fillScreen(0);
-  dt = clock.getDateTime();
-  matrix.setTextColor(0x2E3F);
-  matrix.setCursor(2, 4);
-  matrix.print(clock.dateFormat("H:i", dt));
-  matrix.setTextColor(0x12B8);
-  matrix.setCursor(2, 10);
-  matrix.print(clock.dateFormat("d.m", dt));
+	// if (irrecv.decode(&results)) {
+	//   Serial.println(results.value, HEX);
+	//   irrecv.resume(); // Receive the next value
+   //  }
 
-  matrix.show();
-  delay(60000);
+	matrix.fillScreen(0);
+	dt = clock.getDateTime();
+	matrix.setTextColor(0x2E3F);
+	matrix.setCursor(2, 4);
+	matrix.print(clock.dateFormat("H", dt));
+	matrix.setCursor(10, 4);
+	if ((dt.second % 2) == 1)
+	{
+		matrix.print(':');
+	}
+	matrix.setCursor(12, 4);
+	matrix.print(clock.dateFormat("i", dt));
+	matrix.setTextColor(0x8E84);
+	matrix.setCursor(2, 10);
+	matrix.print(clock.dateFormat("d.m", dt));
+
+	matrix.show();
+	delay(100);
 }
